cccf8fec4e3dc2c308552c1b66bc23ee53b8439c
William <wmcabangon@gmail.com>
2017-09-26 13:31:03 +0100

    added test for number NOT divisible by 3

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -11,4 +11,12 @@ describe('FizzBuzz', function() {
 
   });
 
+  describe('knows when a number is NOT', function() {
+    it('divisible by 3', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+  });
+
 });
