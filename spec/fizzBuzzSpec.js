fd75047a1b3baadde34c42baef0f6383bfcc5214
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-26 13:39:31 +0100

    It knows when a number is and is not divisible by 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,21 +2,26 @@ describe('FizzBuzz', function() {
 
   var fizzBuzz;
 
-  describe('knows when a number is', function() {
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
 
+  describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     });
-
+    it('divisible by 5', function() {
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
 
   describe('knows when a number is NOT', function() {
-    it('divisible by 3', function(){
-      fizzBuzz = new FizzBuzz();
+    it('divisible by 3', function() {
       expect(fizzBuzz.isDivisibleByThree(1)).toBe(false);
     });
-
+    it('divisible by 5', function() {
+      expect(fizzBuzz.isDivisibleByFive(1)).toBe(false);
+    });
   });
 
 });
